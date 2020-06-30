dff1fb60f19eb52645b3b39cd5cdf1e9916a2ecb
Christopher A. Snapp <csnapp@chef.io>
2020-06-30 10:37:19 -0600

    Add windows + linux hab tests to verify pipeline
    This removes the verify/habitat pipeline by bringing windows and linux
    verification tests into the default verify pipeline.
    
    Signed-off-by: Christopher A. Snapp <csnapp@chef.io>

---
+++ habitat/tests/test.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export CHEF_LICENSE="accept-no-persist"
+export HAB_LICENSE="accept-no-persist"
+
+project_root="$(git rev-parse --show-toplevel)"
+pkg_ident="$1"
+
+# print error message followed by usage and exit
+error () {
+  local message="$1"
+
+  echo -e "\nERROR: ${message}\n" >&2
+
+  exit 1
+}
+
+[[ -n "$pkg_ident" ]] || error 'no hab package identity provided'
+
+cd "${project_root}"
+
+echo "--- :mag_right: Testing ${pkg_ident} executables"
+for executable in 'chef-client' 'ohai' 'chef-shell' 'chef-apply' 'knife' 'chef-solo'; do
+	echo -en "\t$executable = "
+  pkg_ident="$pkg_ident" hab pkg exec ${pkg_ident} "${executable}" --version || error "${executable} failed to execute properly"
+done
+
+echo "--- :mag_right: Testing ${pkg_ident} functionality"
+pkg_ident="$pkg_ident" hab pkg exec ${pkg_ident} rspec --format progress --tag ~executables spec/functional || error 'failures during rspec tests'
